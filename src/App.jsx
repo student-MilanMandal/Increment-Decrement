34104fa7366eec8a59657a9331268141101416e4
student-MilanMandal <mandalmilan272@gmail.com>
2024-09-22 13:36:34 +0530

    add updated file

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   }
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
-      <div className="text-[#0398d4] font-medium text-2xl  ">
+      <div className="text-[#04F9F2] font-medium text-2xl  ">
         Increment & Decrement By Milan
       </div>
       <div className="bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151] border-[3px]  hover:border-[#F8D210] hover:rounded-lg ">
